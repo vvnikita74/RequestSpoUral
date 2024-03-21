5f9e1ab0a68b0acd951acb22031dde43f6b69cb8
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-03-22 00:04:54 +0300

    initial

--- next/src/app/layout.jsx
+++ next/src/app/layout.jsx
@@ -1,3 +1,11 @@
+import '@/styles/app.sass'
+
+export const metadata = {
+  title: 'Оставить заявку. СПО-Урал',
+  manifest: '/site.webmanifest'
+}
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
--- next/src/app/page.jsx
+++ next/src/app/page.jsx
@@ -1,3 +1,15 @@
+import Image from "next/image"
+
+
 export default function Home() {
-  return (<>IN DEVELOPMENT</>)
+  return (
+  <>
+  <header className="flex flex-row flex-wrap justify-between items-center">
+    <div className="img-wrapper">
+      <Image src='/images/logo.webp' width={150} height={150} alt="СПО-Урал"/>    
+    </div>
+    <h1>Оставить заявку</h1>
+  </header>
+  </>
+  )
 }
