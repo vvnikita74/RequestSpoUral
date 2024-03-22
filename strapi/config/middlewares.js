d3a3eec27ccd081ea791653e2f1193ae2a708101
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-03-22 14:44:16 +0300

    Update middlewares.js

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -1,7 +1,6 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,17 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
 ];
